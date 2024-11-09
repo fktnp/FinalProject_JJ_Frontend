831eb15bec850ed2c3e8551b575949c83058a7a3
“fluke” <araa1365@gmail.com>
2024-11-09 16:39:14 +0700

    update themepage

--- lib/themepage.dart
+++ lib/themepage.dart
@@ -7,23 +7,86 @@ class Themepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+    final pastelTheme = Theme.of(context).extension<Pastel>()!;
+
     return Scaffold(
+      backgroundColor: pastelTheme.pastel2, 
       appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Theme Settings'),
-            Switch(
-              value: Provider.of<ThemeNotifier>(context).isLightTheme,
-              onChanged: (value) {
-                Provider.of<ThemeNotifier>(context, listen: false)
-                    .toggleTheme();
-              },
+        backgroundColor: pastelTheme.pastel1, 
+        title: Align(
+          alignment: Alignment.centerRight,
+          child: Text(
+            'Theme', 
+            style: TextStyle(
+              fontSize: 20,
+              color: pastelTheme.pastelFont,
             ),
-          ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      if (!themeNotifier.isLightTheme) {
+                        themeNotifier.toggleTheme(); 
+                      }
+                    },
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 255, 220, 188), 
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Defualt', 
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  GestureDetector(
+                    onTap: () {
+                      if (themeNotifier.isLightTheme) {
+                        themeNotifier.toggleTheme(); 
+                      }
+                    },
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.black, 
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Dark', 
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
